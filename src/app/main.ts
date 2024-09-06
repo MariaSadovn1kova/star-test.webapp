66c22d155db8400908fbbc81b0555017ee959461
Maria <msadovnikov904@gmail.com>
2024-09-06 13:53:26 +0300

    init: project structure

---
+++ src/app/main.ts
@@ -0,0 +1,17 @@
+import './styles/main.pcss';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+import i18n from '@/shared/lang';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(i18n)
+
+app.mount('#app')
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+</script>
+
+<template>
+  <main>
+    <div>{{ t('btns.report_settings') }}</div>
+  </main>
+</template>
